50be74cb5c80d3a835db60e3e217587da12c6450
dss <digital.synapse.software@gmail.com>
2019-03-19 20:40:44 -0700

    support scanned network in captive portal

--- WiFiSetup.cpp
+++ WiFiSetup.cpp
@@ -7,53 +7,12 @@ IPAddress         apIP(10, 10, 10, 1);    // Private network for server
 DNSServer         dnsServer;              // Create the DNS object
 ESP8266WebServer  webServer(80);          // HTTP server
 
-/*
-String loginHTML = ""
-"<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body>"
-"<h1>Device Setup</h1><p>Please enter your wirless network info below."
-"</p><form action=\"/\" method=\"post\">"
-"<input type=\"text\" placeholder=\"Enter SSID\" name=\"ssid\" required>"
-"<input type=\"text\" placeholder=\"Enter PASSWORD\" name=\"pass\" required>"
-"<button type=\"submit\" value=\"Send\">Login</button>"
-"</form></body></html>";
-*/
-String loginHTML = ""
-"<!DOCTYPE html><html><head><title>CaptivePortal</title>"
-"<style>"
-"	div{ padding-bottom: 10px; }"
-"	.tc_settings { display: none; required: false; }"
-"	td, h1, h2, h3, h4, h5, p, ul, ol, li{page-break-inside: avoid;}body{font-size: 12px; font-family: Helvetica, Arial;}h1, h2{border-bottom: 1px solid #DDDDDD;}pre, code{font-family: Consolas, 'Liberation Mono', Courier; background-color: #F4F4F4; border: 1px solid #EAEAEA; border-radius: 2px; padding: 5px;}"
-"	td { padding-right: 10px; vertical-align: top; }"
-"	input[type='checkbox'] { vertical-align: middle; }"
-"</style>"
-"<script>"
-"function show_tc_settings(){"
-"	var settings =document.getElementsByClassName('tc_settings');"
-"	for (var i=0; i<settings.length; i++){"
-"		var setting = settings[i];"
-"		if (document.getElementById('cb_tc').checked){"
-"			setting.style.display='block';"
-"			setting.required=true;"
-"		}"
-"		else{"
-"			setting.style.display='none';"
-"			setting.required=false;"
-"		}"
-"	}"
-"}"
-"</script>"
-"</head><body>"
-"<h1>Device Setup</h1><p>Please enter your wirless network info below."
-"</p><small><form action='/' method='post'>"
-"<div>Wifi SSID<br/><input type='text' name='ssid' required></div>"
-"<div>Wifi Password<br/><input type='text' name='pass' required></div>"
-"<br /><button type='submit' value='Send'>Login</button>"
-"</form></small></body></html>";
-
-String submitHTML = ""
-"<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body>"
-"<h1>Thank You!</h1><p>Please wait while the device attempts to connect to the AP...</p>"
-"</body></html>";
+String loginHTML1 = "<!DOCTYPE html><html><head><title>CaptivePortal</title>"
+"<style> div{ padding-bottom: 10px; } .tc_settings { display: none; required: false; } td, h1, h2, h3, h4, h5, p, ul, ol, li{page-break-inside: avoid;}body{font-size: 12px; font-family: Helvetica, Arial;}h1, h2{border-bottom: 1px solid #DDDDDD;}pre, code{font-family: Consolas, 'Liberation Mono', Courier; background-color: #F4F4F4; border: 1px solid #EAEAEA; border-radius: 2px; padding: 5px;} td { padding-right: 10px; vertical-align: top; } input[type='checkbox'] { vertical-align: middle; }</style>"
+"<script> (function() {function getSelectedOption(sel) { var opt; for ( var i = 0, len = sel.options.length; i < len; i++ ) { opt = sel.options[i]; if ( opt.selected === true ) { break; }} return opt; } document.addEventListener('DOMContentLoaded', function(event) { var ssids =document.getElementById('ssids'); var ssid =document.getElementById('ssid'); ssid.style.display='none'; ssids.onchange = function () { var opt = getSelectedOption(ssids); if (opt.value == 'custom') ssid.style.display='block'; else { ssid.style.display='none'; ssid.value = opt.value; } }; ssids.onchange(); }); })(); </script>"
+"</head><body><h1>Device Setup</h1><p>Please enter your wirless network info below.</p><small><form action='/' method='post'><div>Wifi SSID<br/><select id='ssids' name='ssids' required>";
+String loginHTML2 = "<option value='custom'>Enter Custom</option></select></div><div><input type='text' id='ssid' name='ssid' required></div><div>Wifi Password<br/><input type='text' name='pass' required></div><br /><button type='submit' value='Send'>Login</button></form></small></body></html>";
+String submitHTML = "<!DOCTYPE html><html><head><title>CaptivePortal</title></head><body><h1>Thank You!</h1><p>Please wait while the device attempts to connect to the AP...</p></body></html>";
 
 ESP8266WebServer* getWebServer() {
 	return &webServer;
@@ -66,14 +25,12 @@ String connectWiFi() {
 	bool flash = digitalRead(0)==0;
 	String ssid = flash ? "" : readString(0);
 	String pass = flash ? "" : readString(1);
-
 	if (ssid == "" || pass == "") return "";
 
 	WiFi.begin(ssid, pass);             // Connect to the network
 	Serial.print("Connecting to ");
 	Serial.print(ssid); Serial.println(" ...");
-	//Serial.println(pass);
-
+	
 	// Wait 10 seconds for the Wi-Fi to connect
 	int i = 0;
 	while (WiFi.status() != WL_CONNECTED && i < 10) {
@@ -95,7 +52,24 @@ String connectWiFi() {
 	return "";
 }
 
+String options = "";
 bool connectAPModeCaptiveNetworkServer() {
+	
+	// WIFI onnection failed, scan for SSIDs	
+	Serial.println("scanning networks...");		
+	int numberOfNetworks = WiFi.scanNetworks();		
+	int maxSignal = -100;
+	String option;
+	for (int i = 0; i < numberOfNetworks; i++) {
+		int signal = WiFi.RSSI(i);
+		option = "<option value='" + WiFi.SSID(i) + "'>" + WiFi.SSID(i) +/*" " + String(signal) + "dBm*/"</option>";
+		if (signal > maxSignal) {
+			options =  option + options;
+			maxSignal = signal;
+		}
+		else options = options + option;
+	}	
+
 	// WIFI connection failed, start AP mode
 	Serial.println("starting AP mode...");
 	WiFi.mode(WIFI_AP);
@@ -116,51 +90,14 @@ bool connectAPModeCaptiveNetworkServer() {
 		Serial.println("pass: " + pass);
 		writeString(ssid, 0);
 		writeString(pass, 1);
-
-		// get teamcity settings
-		String tc_ip = webServer.arg("tc_ip");
-		Serial.println("tc_ip: " + tc_ip);
-		writeString(tc_ip, 2);
-		if (tc_ip != "") {
-			String tc_user = webServer.arg("tc_user");
-			String tc_pass = webServer.arg("tc_pass");
-			Serial.println("tc_user: " + tc_user);
-			Serial.println("tc_pass: " + tc_pass);
-			base64 b64;
-			String tc_auth = b64.encode(tc_user + ":" + tc_pass, false);
-			writeString(tc_auth, 3);
-			String tc_project = webServer.arg("tc_project");
-			Serial.println("tc_project: " + tc_project);
-			writeString(tc_project, 4);
-
-			int freq = webServer.arg("tc_frequency").toInt();
-			if (freq == 0) freq = 60;
-			Serial.println("tc_frequency: " + String(freq));
-			writeInt(freq, 7);
-			int timeout = webServer.arg("tc_timeout").toInt();
-			Serial.println("tc_timeout: " + String(timeout));
-			writeInt(timeout, 8);
-
-			bool enableGreen = webServer.arg("tc_enable_green") == "on";
-			Serial.println("tc_enable_green: " + String(enableGreen));
-			bool enableYellow = webServer.arg("tc_enable_yellow") == "on";
-			Serial.println("tc_enable_yellow: " + String(enableYellow));
-			bool enableRed = webServer.arg("tc_enable_red") == "on";
-			Serial.println("tc_enable_reed: " + String(enableRed));
-			uint8 flags = 0;
-			if (enableGreen) flags |= 0x1;
-			if (enableYellow) flags |= 0x2;
-			if (enableRed) flags |= 0x4;
-			writeByte(flags, 0);
-		}
-
+		
 		delay(2000);
 		reboot();
 	});
 
 	// replay to all requests with same HTML
 	webServer.onNotFound([]() {
-		webServer.send(200, "text/html", loginHTML);
+		webServer.send(200, "text/html", loginHTML1 + options + loginHTML2);
 	});
 
 	Serial.println("starting web server...");
